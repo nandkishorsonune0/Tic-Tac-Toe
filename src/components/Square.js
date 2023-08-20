b96592c18571922ae39c7a9279e9d3acfabc5a37
Nandkishor Sonune <nandkishorsonune0@gmail.com>
2023-08-20 20:22:07 +0530

    Full Fuunctionality Uploaded

---
+++ src/components/Square.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Square = ({ value, onClick, isWinningSquare }) => {
+  return (
+    <button
+      type="button"
+      // aria-label={true}
+      onClick={onClick}
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
